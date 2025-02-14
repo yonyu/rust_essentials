64fb5b4526214cb223b9aa40711e0bba08a34f3c
Yong Yu <ypyu@hotmail.com>
2025-02-13 17:07:26 -0800

    Restructure the packages - move files around

---
+++ crates/smart_pointers/src/lib.rs
@@ -0,0 +1,19 @@
+mod smart_pointers;
+
+pub mod mybox;
+pub mod recursive_type_with_box;
+pub mod custom_smart_pointer;
+pub mod reference_counting;
+pub mod limited_tracker;
+pub mod multiple_owners_of_mutable_data;
+pub mod reference_cycles;
+pub mod weak_references;
+
+
+//use smart_pointers::recursive_type_with_box::List::{Cons, Nil};
+
+fn main() {
+    //smart_pointers::recursive_type_with_box::instantiate_list();
+
+    reference_cycles::demo_cycles();
+}
--- src/smart_pointers.rs
+++
@@ -1,8 +0,0 @@
-pub mod mybox;
-pub mod recursive_type_with_box;
-pub mod custom_smart_pointer;
-pub mod reference_counting;
-pub mod limited_tracker;
-pub mod multiple_owners_of_mutable_data;
-pub mod reference_cycles;
-pub mod weak_references;
