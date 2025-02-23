b9d23eec1d75473ddefc3c3cfea4527f7197b53b
Yong Yu <ypyu@hotmail.com>
2025-02-23 14:42:02 -0800

    Tune the code

--- crates/concurrency/src/async_await/timeout_async.rs
+++ crates/concurrency/src/async_await/timeout_async.rs
@@ -5,11 +5,11 @@ fn exercise_timeout() {
 
     trpl::run(async {
         let slow = async {
-            trpl::sleep(Duration::from_millis(100)).await;  // 5
-            "I finished"
+            trpl::sleep(Duration::from_secs(5)).await;
+            "Finally finished"
         };
 
-        match timeout(slow, Duration::from_millis(10)).await {  // 2
+        match timeout(slow, Duration::from_secs(2)).await {
             Ok(message) => println!("Succeeded with '{message}'"),
             Err(duration) => {
                 println!("Failed after {} seconds", duration.as_secs())
@@ -20,7 +20,7 @@ fn exercise_timeout() {
 
 async fn timeout<F: Future> (
     future_to_try: F,
-    max_time: Duration
+    max_time: Duration,
 ) -> Result<F::Output, Duration> {
     match trpl::race(future_to_try, trpl::sleep(max_time)).await {
         Either::Left(output) => Ok(output),
